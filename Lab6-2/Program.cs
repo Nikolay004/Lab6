50a90ff0d729864ceb6eae99d3a24456e5947aca
Ніколай Антонов <antonooff_2nic@mail.ru>
2021-11-29 20:26:00 +0800

    Добавьте файлы проекта.

---
+++ Lab6-2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string S = Console.ReadLine();
+            S = S.Replace(" ", string.Empty);
+
+            char[] ar = S.ToCharArray();
+            Array.Reverse(ar);
+            string revS = new String(ar);
+
+            if (S == revS)
+            {
+                Console.WriteLine("Данное предложение является перевртышем");
+            }
+            else
+            {
+                Console.WriteLine("Данное предложение не является перевртышем");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Lab6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string startString = Console.ReadLine();
+            string[] words = startString.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int max = words[0].Length, min = words[0].Length;
+            foreach (string str in words)
+            {
+                if (str.Length > max) max = str.Length;
+            }
+            Console.WriteLine("Длина самого длинного слова: " + max);
+            Console.ReadLine();
+        }
+    }
+}
